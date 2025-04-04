19bf53e792caa9aa9ed3a3bad281ab11ad68cdd7
jojunhyeong <jojh0323@naver.com>
2025-04-04 15:16:01 +0900

    feat) make post ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { TopNav } from "./components/common/TopNav";
 import { Home } from "./components/main/Home";
 import { NoticeBoard } from "./components/main/NoticeBoard";
+import { Post } from "./components/post/Post";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/notice" element={<NoticeBoard />} />
+        <Route path="/post/:id" element={<Post />} />
       </Routes>
     </Router>
   );
